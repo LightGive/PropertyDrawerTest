a25105de9bc13eb8374d4463fe68bd5bf67d1e6d
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-11 17:43:52 +0900

    テストコード追加

---
+++ PropertyDrawerTest/Assets/Scripts/Test.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	[SerializeField, Piyo]
+	private List<Piyo> piyo = new List<Piyo>();
+}
