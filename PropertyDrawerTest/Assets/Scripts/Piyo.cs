5e3849fe3cd4485d96db9be0439a87af8718924e
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-11 17:55:11 +0900

    コード整理

--- PropertyDrawerTest/Assets/Scripts/Piyo.cs
+++ PropertyDrawerTest/Assets/Scripts/Piyo.cs
@@ -1,13 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
+﻿[System.Serializable]
 public class Piyo
 {
-	public bool isButton;
-	public Piyo()
-	{
 
-	}
 }
